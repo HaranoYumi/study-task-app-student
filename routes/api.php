8656152618f21559bf153b976feb26ac5c8770a0
e-value <y.miyata@env-value.co.jp>
2026-01-24 14:00:55 +0900

    テスト練習用のTestController関連ファイルを削除

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\Api\ProjectMemberController;
 use App\Http\Controllers\Api\ProjectController;
 use App\Http\Controllers\Api\TaskController;
 use App\Http\Controllers\Api\UserController;
-use App\Http\Controllers\Api\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,10 +15,6 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 Route::get('/test', function () {
     throw new \Exception("テストエラー");
 });
-
-// テスト練習用（認証なし）
-Route::get('/test/ping', [TestController::class, 'ping']);
-Route::post('/test/echo', [TestController::class, 'echo']);
 
 // Projects
 Route::get('/test/projects', [ProjectController::class, 'index']);
