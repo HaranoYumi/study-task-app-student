4d9a0b987128cfc13e4be1e44a978f6c1e4eabcb
e-value <y.miyata@env-value.co.jp>
2026-01-25 12:12:31 +0900

    Add HasFactory trait to Membership model

--- app/Models/Membership.php
+++ app/Models/Membership.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Relations\Pivot;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Membership extends Pivot
 {
+    use HasFactory;
+
     /**
      * The table associated with the model.
      *
